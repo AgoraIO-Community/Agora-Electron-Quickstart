8f602ba29f67afc76a520b668be4e22739be8576
Hao Yang <tillohao@gmail.com>
2018-06-28 11:17:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Agora Electron Quickstart
+
+## Introduction
+A **very** simple quickstart boilerplate for using [Agora-RTC-SDK-for-Electron](https://github.com/AgoraIO-Community/Agora-RTC-SDK-for-Electron). Building with [React](https://github.com/facebook/react)  and [electron-webpack](https://github.com/electron-userland/electron-webpack).
+
+
+## How to build
+
+``` bash
+# install dependencies
+npm install 
+# enable dynamic compiling and HMR developing enviroment
+npm run dev
+# build for release
+npm run dist
+```
---
+++ src/utils/settings.js
@@ -0,0 +1,76 @@
+export const videoProfileList = [
+  {
+    value: 0,
+    label: '160x120	15fps	65bit'
+  },
+  {
+    value: 20,
+    label: '320x240	15fps	200bit'
+  },
+  {
+    value: 30,
+    label: '640x360	15fps	400bit'
+  },
+  {
+    value: 43,
+    label: '640x480	30fps	750bit'
+  },
+  {
+    value: 50,
+    label: '1280x720 15fps 1130bit'
+  }
+]
+
+export const audioProfileList = [
+  {
+    value: 0,
+    label: 'AUDIO_PROFILE_DEFAULT'
+  },
+  {
+    value: 1,
+    label: 'AUDIO_PROFILE_SPEECH_STANDARD'
+  },
+  {
+    value: 2,
+    label: 'AUDIO_PROFILE_MUSIC_STANDARD'
+  },
+  {
+    value: 3,
+    label: 'AUDIO_PROFILE_MUSIC_STANDARD_STEREO'
+  },
+  {
+    value: 4,
+   label: 'AUDIO_PROFILE_MUSIC_HIGH_QUALITY'
+  },
+  {
+    value: 5,
+    label: 'AUDIO_PROFILE_MUSIC_HIGH_QUALITY_STEREO'
+  } 
+]
+
+export const audioScenarioList = [
+  {
+    value: 0,
+    label: 'AUDIO_PROFILE_DEFAULT'
+  },
+  {
+    value: 1,
+    label: 'AUDIO_SCENARIO_CHATROOM_ENTERTAINMENT'
+  },
+  {
+    value: 2,
+    label: 'AUDIO_SCENARIO_EDUCATION'
+  },
+  {
+    value: 3,
+    label: 'AUDIO_SCENARIO_GAME_STREAMING'
+  },
+  {
+    value: 4,
+   label: 'AUDIO_SCENARIO_SHOWROOM'
+  },
+  {
+    value: 5,
+    label: 'AUDIO_SCENARIO_CHATROOM_GAMING'
+  } 
+]
