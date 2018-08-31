b6322edb5048a5d00379690c82d0dc7d9df9dfec
Hao Yang <tillohao@gmail.com>
2018-08-31 11:10:24 +0800

    remove useless lib and modify dependencies (sass related and webpack related)

--- webpack.renderer.additions.js
+++ webpack.renderer.additions.js
@@ -1,26 +1,3 @@
-// temp workaround for https://github.com/electron-userland/electron-webpack/issues/125
-const presetEnv = require('@babel/preset-env/lib/index.js');
-delete presetEnv.default;
-delete presetEnv.isPluginRequired;
-delete presetEnv.transformIncludesAndExcludes;
-
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.s[a|c]ss$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader"
-          },
-          {
-            loader: "sass-loader"
-          }
-        ]
-      }
-    ]
-  }
+
 }
