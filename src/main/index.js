3058e136edc5347246a1a5e478eb565d0b7cd204
张乾泽 <qianze.zhang@hotmail.com>
2019-10-22 22:02:46 +0800

    turn on webdevtools

--- src/main/index.js
+++ src/main/index.js
@@ -16,9 +16,9 @@ function createMainWindow() {
     webPreferences: {nodeIntegration: true}
   })
 
-  if (isDevelopment) {
+  // if (isDevelopment) {
     window.webContents.openDevTools()
-  }
+  // }
 
   if (isDevelopment) {
     window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
