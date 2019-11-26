7c66e134fadf09a56c3f5727464d39f83c6eb842
QianzeZhang <zhangqianze@agora.io>
2019-11-26 12:08:08 +0800

    fix script

--- script/writeAppId.js
+++ script/writeAppId.js
@@ -8,7 +8,7 @@ const file = fs.readFileSync(targetPath, 'utf8');
 const result = file.replace(/global.AGORA_APPID = (.*)/, `global.AGORA_APPID = "${process.env.AGORA_APPID}"`);
 
 if (process.env.AGORA_APPID) {
-  console.log('======= Agora App ID has been set  =====')
+  console.log(`======= Agora App ID ${process.env.AGORA_APPID} has been set  =====`)
 } else {
   console.log('======= Agora App ID is empty  =====')
 }
