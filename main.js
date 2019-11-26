695de421707376f85c1a2353aa881ea99d400685
QianzeZhang <zhangqianze@agora.io>
2019-11-26 14:01:07 +0800

    disable auto updater

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-require('update-electron-app')({
-  logger: require('electron-log')
-})
-
 const path = require('path')
 const glob = require('glob')
 const {app, BrowserWindow} = require('electron')
