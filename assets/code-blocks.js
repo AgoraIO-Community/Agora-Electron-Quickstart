b9a318b90fb03bd9fad80ceb30bda54350b97738
QianzeZhang <zhangqianze@agora.io>
2019-11-30 02:47:55 +0800

    add code editor

--- assets/code-blocks.js
+++ assets/code-blocks.js
@@ -1,7 +1,10 @@
 const fs = require('fs')
 const path = require('path')
-
+const ace = require('./ace/ace')
 const codeBlocksWithPaths = document.querySelectorAll('code[data-path]')
+const {ipcRenderer} = require('electron')
+
+ace.config.set('basePath', path.join(__dirname, './ace'))
 
 Array.prototype.forEach.call(codeBlocksWithPaths, (code) => {
   const codePath = path.join(__dirname, '..', code.dataset.path)
@@ -10,10 +13,24 @@ Array.prototype.forEach.call(codeBlocksWithPaths, (code) => {
   code.textContent = fs.readFileSync(codePath)
 })
 
+ipcRenderer.on('exec-source-error', (event, arg) => {
+  debugger
+})
+
 document.addEventListener('DOMContentLoaded', () => {
-  const highlight = require('highlight.js')
-  const codeBlocks = document.querySelectorAll('pre code')
-  Array.prototype.forEach.call(codeBlocks, (code) => {
-    highlight.highlightBlock(code)
+  const editors = document.querySelectorAll('.editor')
+  Array.prototype.forEach.call(editors, (editorDom) => {
+    let editor = ace.edit(`${editorDom.id}`);
+    editor.setTheme("ace/theme/textmate");
+    editor.session.setMode("ace/mode/javascript");
+    editor.setOption('maxLines', Infinity);
+
+    let demoBtn = document.querySelector(`.demo-button[data-target="${editorDom.id}"]`)
+    demoBtn.addEventListener('click', () => {
+      ipcRenderer.send('exec-source', {
+        code: editor.getValue(),
+        id: editorDom.id
+      })
+    })
   })
 })
--- renderer-process/video-audio/join-channel.js
+++ renderer-process/video-audio/join-channel.js
@@ -1,27 +1,26 @@
-const AgoraRtcEngine = require('agora-electron-sdk').default
+(() => {
+  const AgoraRtcEngine = require('agora-electron-sdk').default
 
-const os = require('os')
-const path = require('path')
-const consoleContainer = document.getElementById('agora-join-channel-console')
-const sdkLogPath = path.resolve(os.homedir(), "./agoramainsdk.log")
-const joinChannelBtn = document.getElementById('agora-join-channel')
-const localVideoContainer = document.getElementById('join-channel-local-video')
-const remoteVideoContainer = document.getElementById('join-channel-remote-video')
-const APPID = global.AGORA_APPID || ""
-
-joinChannelBtn.addEventListener('click', () => {
+  const os = require('os')
+  const path = require('path')
+  const consoleContainer = document.getElementById('agora-join-channel-console')
+  const sdkLogPath = path.resolve(os.homedir(), "./agoramainsdk.log")
+  const localVideoContainer = document.getElementById('join-channel-local-video')
+  const remoteVideoContainer = document.getElementById('join-channel-remote-video')
+  const APPID = global.AGORA_APPID || ""
+  
   if(!APPID) {
     alert(`AGORA_APPID not found in environment variables`)
   }
-
+  
   if(global.rtcEngine) {
     // if rtc engine exists already, you must call release to free it first
     global.rtcEngine.release()
   }
-
+  
   let rtcEngine = new AgoraRtcEngine()
   rtcEngine.initialize(APPID)
-
+  
   // listen to events
   rtcEngine.on('joinedChannel', (channel, uid, elapsed) => {
     consoleContainer.innerHTML = `joined channel ${channel} with uid ${uid}, elapsed ${elapsed}ms`
@@ -36,20 +35,19 @@ joinChannelBtn.addEventListener('click', () => {
     rtcEngine.setupViewContentMode(uid, 1);
     rtcEngine.subscribe(uid, remoteVideoContainer)
   })
-
+  
   // set channel profile, 0: video call, 1: live broadcasting
   rtcEngine.setChannelProfile(1)
   rtcEngine.setClientRole(1)
-
+  
   // enable video, call disableVideo() is you don't need video at all
   rtcEngine.enableVideo()
-
+  
   // set where log file should be put for problem diagnostic
   rtcEngine.setLogFile(sdkLogPath)
-
+  
   // join channel to rock!
   rtcEngine.joinChannel(null, "demoChannel", null, Math.floor(new Date().getTime() / 1000))
-
-  global.rtcEngine = rtcEngine
-})
-
+  
+  global.rtcEngine = rtcEngine  
+})()
