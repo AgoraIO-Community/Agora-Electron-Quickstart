5c495e6d4d5b15ea1a6b4c25b9a76851634b5508
QianzeZhang <zhangqianze@agora.io>
2020-02-28 21:36:26 +0800

    fix

--- scripts/writeAppId.js
+++ scripts/writeAppId.js
@@ -5,7 +5,7 @@ const targetPath = path.resolve(__dirname, '../src/utils/settings.js')
 
 const file = fs.readFileSync(targetPath, 'utf8');
 
-const result = file.replace(/export const APP_ID = '(.*)'/, `export const APP_ID = ${process.env.AGORA_APP_ID}`);
+const result = file.replace(/export const APP_ID = '(.*)'/, `export const APP_ID = "${process.env.AGORA_APP_ID}"`);
 
 if (process.env.AGORA_APP_ID) {
   console.log('======= Agora App ID has been set  =====')
