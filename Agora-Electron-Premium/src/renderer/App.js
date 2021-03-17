3585d99b54afd58cba1c37e985faee0f13dabb89
zhangtao <zhangtao@agora.io>
2021-03-17 17:52:25 +0800

    update premium

--- Agora-Electron-Premium/src/renderer/App.js
+++ Agora-Electron-Premium/src/renderer/App.js
@@ -17,7 +17,7 @@ export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      appid: ,
+      appid: '',
       token: '',
       local: '',
       localVideoSource: '',
