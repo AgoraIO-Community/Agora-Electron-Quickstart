75e7f6f3a05eaaa71bafe8b6748849441587bbad
Luohao <luohao@agora.io>
2021-09-23 16:20:02 +0800

    test api

--- Agora-Electron-Premium/src/renderer/components/Window.js
+++ Agora-Electron-Premium/src/renderer/components/Window.js
@@ -10,21 +10,25 @@ export default class Window extends Component {
   }
 
   componentDidMount() {
-    const { role, uid } = this.props
-    console.log('windows  this.props.role:', role, '     this.props.uid:', uid)
+    const { role, uid,channelId } = this.props;
+  
+    console.log('windows  this.props.role:', role, '     this.props.uid:', uid,channelId)
     let dom = document.querySelector(`#video-${uid}`)
     if (this.props.role === 'local') {
+      console.warn('local-----')
       dom && this.props.rtcEngine.setupLocalView(0, 0, dom)
       this.props.rtcEngine.setupLocalViewContentMode(0, 0, 1)
     } else if (this.props.role === 'localVideoSource') {
-      console.log('localVideoSource-----')
+      console.warn('localVideoSource-----')
       dom && this.props.rtcEngine.setupLocalView(3, 0, dom)
       this.props.rtcEngine.setupLocalViewContentMode(3, 0, 1)
     } else if (this.props.role === 'remote') {
-      dom && this.props.rtcEngine.setupRemoteView(uid, 0, dom)
+      console.warn('remote-----')
+      dom && this.props.rtcEngine.setupRemoteView(uid, channelId, dom)
       this.props.rtcEngine.setupRemoteViewContentMode(uid, 0, 1)
     } else if (this.props.role === 'remoteVideoSource') {
-      dom && this.props.rtcEngine.setupRemoteView(uid, 0, dom)
+      console.warn('remoteVideoSource-----')
+      dom && this.props.rtcEngine.setupRemoteView(uid, channelId, dom)
       this.props.rtcEngine.setupRemoteViewContentMode(String(SHARE_ID), 0, 1)
     }
   }
