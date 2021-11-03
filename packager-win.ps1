acc74066ddf5a7bae78fafb69c10a4242062ea1e
Luohao <luohao@agora.io>
2021-11-03 15:00:33 +0800

    update windows ci

--- packager-win.ps1
+++ packager-win.ps1
@@ -32,25 +32,7 @@ function distByArch($type)
 switch -Regex ($chooseExampleType)
 {
     1 {
-      pushd Agora-Electron-Premium
-      # choose arch
-      ChooseArch -type $args[1]
-      # remove node_modules
-      Remove-Item -Path node_modules -Recurse -Force -ErrorAction Ignore;
-      # remove dist
-      Remove-Item -Path dist -Recurse -Force -ErrorAction Ignore;
-      yarn
-      # copy native sdk
-      Copy-Item -Path ../Electron-*/* -Destination ./node_modules/agora-electron-sdk/ -Recurse -Force
-      # dist start
-      DistByArch -type $args[1]
-      # move zip
-      Copy-Item -Path dist/agora-electron-*.zip -Destination ../$outterZipName -Recurse -Force
-      popd;
-      Break
-    }
-    2 {
-      pushd Agora-Electron-Iris
+      pushd Agora-Electron-API-Example
       # choose arch
       ChooseArch -type $args[1]
       # remove node_modules
@@ -69,7 +51,44 @@ switch -Regex ($chooseExampleType)
       popd;
       Break
     }
-    3 {"It is three."; Break}
+    2 {
+      pushd Agora-Electron-API-Example-Iris
+      # choose arch
+      ChooseArch -type $args[1]
+      # remove node_modules
+      Remove-Item -Path node_modules -Recurse -Force -ErrorAction Ignore;
+      # remove node_modules
+      Remove-Item -Path src/node_modules -Recurse -Force -ErrorAction Ignore;
+      # remove dist
+      Remove-Item -Path release -Recurse -Force -ErrorAction Ignore;
+      yarn
+      # copy native sdk
+      Copy-Item -Path ../Electron-*/* -Destination src/node_modules/agora-electron-sdk/ -Recurse -Force
+      # dist start
+      DistByArch -type $args[1]
+      # move zip
+      Copy-Item -Path release/ElectronReact-*.zip -Destination ../$outterZipName -Recurse -Force
+      popd;
+      Break
+    }
+    3 {
+      pushd Agora-Electron-Premium
+      # choose arch
+      ChooseArch -type $args[1]
+      # remove node_modules
+      Remove-Item -Path node_modules -Recurse -Force -ErrorAction Ignore;
+      # remove dist
+      Remove-Item -Path dist -Recurse -Force -ErrorAction Ignore;
+      yarn
+      # copy native sdk
+      Copy-Item -Path ../Electron-*/* -Destination ./node_modules/agora-electron-sdk/ -Recurse -Force
+      # dist start
+      DistByArch -type $args[1]
+      # move zip
+      Copy-Item -Path dist/agora-electron-*.zip -Destination ../$outterZipName -Recurse -Force
+      popd;
+      Break
+    }
     4 {"It is four."; Break}
 }
 
