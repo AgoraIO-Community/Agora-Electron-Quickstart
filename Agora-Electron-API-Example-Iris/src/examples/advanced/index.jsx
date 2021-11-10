82972f8f8a3317980058a1c1eeb0c513f1825063
Jerry.Luo <396912848@qq.com>
2021-11-10 21:09:04 +0800

    move example (#90)

--- Agora-Electron-API-Example-Iris/src/examples/advanced/index.jsx
+++ Agora-Electron-API-Example-Iris/src/examples/advanced/index.jsx
@@ -1,4 +1,7 @@
 import ScreenShare from './ScreenShare/ScreenShare';
+// import LoadPlugin from './LoadPlugin';
+import ChannelMediaRelay from './ChannelMediaRelay';
+import CreateDataStream from './CreateDataStream/CreateDataStream';
 import JoinMultipleChannel from './JoinMultipleChannel/JoinMultipleChannel';
 
 const advanceRoute = [
@@ -6,6 +9,21 @@ const advanceRoute = [
     path: '/ScreenShare',
     component: ScreenShare,
     title: 'ScreenShare',
+  },
+  // {
+  //   path: '/LoadPlugin',
+  //   component: LoadPlugin,
+  //   title: 'LoadPlugin',
+  // },
+  {
+    path: '/ChannelMediaRelay',
+    component: ChannelMediaRelay,
+    title: 'ChannelMediaRelay',
+  },
+  {
+    path: '/CreateDataStream',
+    component: CreateDataStream,
+    title: 'CreateDataStream',
   },
   {
     path: '/JoinMultipleChannel',
