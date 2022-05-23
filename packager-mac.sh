73e4f4859fece7219e5becfd7cc010c88d840d0e
Jerry.Luo <396912848@qq.com>
2022-05-23 16:54:18 +0800

    fix mac build

--- packager-mac.sh
+++ packager-mac.sh
@@ -1,50 +1,33 @@
 chooseExampleType=$1
 outterZipName="electronDemo.zip"
 
-packOldPremium() {
-  pushd Agora-Electron-Premium
-  echo 当前工作路径:$(pwd)
-  rm -rf node_modules
-  yarn
-  cp -P -R ../Electron-*/* node_modules/agora-electron-sdk/
-  yarn dist:zip
-
-  pushd dist/mac
-  zip -ry $(pwd)/../../../${outterZipName} agora-electron.app
-  popd
-  popd
-}
-
 packExample() {
+  rm $outterZipName
   pushd $1
   echo 当前工作路径:$(pwd)
-  rm -rf node_modules
-  rm -rf src/node_modules
+  rm -rf node_modules dist
   yarn
   cp -P -R ../Electron-*/* src/node_modules/agora-electron-sdk/
-  yarn package-mac
+  yarn dist:mac
 
-  pushd release/mac
+  pushd dist/mac
   zip -ry $(pwd)/../../../${outterZipName} ElectronReact.app
   popd
-
   popd
 }
-
-
 
 case $chooseExampleType in
 1)
-  echo '打包: API-Example'
+  echo 'package mac: 1'
   packExample Agora-Electron-API-Example
   ;;
 2)
-  echo '打包: Iris'
-  packExample Agora-Electron-API-Example-Iris
+  echo 'package mac: 2'
+  packExample Agora-Electron-API-Example
   ;;
 3)
-  echo '你选择了 3'
-  packOldPremium
+  echo 'package mac:3 '
+  packExample Agora-Electron-API-Example
   ;;
 4)
   echo '你选择了 4'
