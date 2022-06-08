2c2286915755df4cdc388f97c1547ffa61766214
Jerry.Luo <396912848@qq.com>
2022-06-08 20:53:46 +0800

    update mpk

--- Agora-Electron-API-Example/src/renderer/examples/advanced/MediaPlayer/MediaPlayer.tsx
+++ Agora-Electron-API-Example/src/renderer/examples/advanced/MediaPlayer/MediaPlayer.tsx
@@ -1,18 +1,14 @@
 import creteAgoraRtcEngine, {
   IMediaPlayer,
+  IMediaPlayerSourceObserver,
   IRtcEngine,
-  IRtcEngineEventHandlerEx,
   IRtcEngineEx,
   MediaPlayerError,
   MediaPlayerState,
-  RtcConnection,
   RtcEngineExImplInternal,
-  RtcStats,
-  UserOfflineReasonType,
   VideoSourceType,
 } from 'agora-electron-sdk'
-import { IMediaPlayerSourceObserver } from 'agora-electron-sdk/types/Private/IAgoraMediaPlayerSource'
-import { Input } from 'antd'
+import { Button, Input } from 'antd'
 import { Component } from 'react'
 import Window from '../../component/Window'
 import config from '../../config/agora.config'
@@ -84,69 +80,6 @@ export default class MediaPlayer
     this.setState({ mpkState: state })
   }
 
-  // onJoinChannelSuccessEx(
-  //   { channelId, localUid }: RtcConnection,
-  //   elapsed: number
-  // ): void {
-  //   try {
-  //     const { allUser: oldAllUser } = this.state
-  //     const newAllUser = [...oldAllUser]
-  //     newAllUser.push({ isMyself: true, uid: localUid })
-  //     this.setState({
-  //       isJoined: true,
-  //       allUser: newAllUser,
-  //     })
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
-
-  // onUserJoinedEx(
-  //   connection: RtcConnection,
-  //   remoteUid: number,
-  //   elapsed: number
-  // ): void {
-  //   console.log(
-  //     'onUserJoinedEx',
-  //     'connection',
-  //     connection,
-  //     'remoteUid',
-  //     remoteUid
-  //   )
-
-  //   const { allUser: oldAllUser } = this.state
-  //   const newAllUser = [...oldAllUser]
-  //   newAllUser.push({ isMyself: false, uid: remoteUid })
-  //   this.setState({
-  //     allUser: newAllUser,
-  //   })
-  // }
-
-  // onUserOfflineEx(
-  //   { localUid, channelId }: RtcConnection,
-  //   remoteUid: number,
-  //   reason: UserOfflineReasonType
-  // ): void {
-  //   console.log('onUserOfflineEx', channelId, remoteUid)
-
-  //   const { allUser: oldAllUser } = this.state
-  //   const newAllUser = [...oldAllUser.filter((obj) => obj.uid !== remoteUid)]
-  //   this.setState({
-  //     allUser: newAllUser,
-  //   })
-  // }
-
-  // onLeaveChannelEx(connection: RtcConnection, stats: RtcStats): void {
-  //   this.setState({
-  //     isJoined: false,
-  //     allUser: [],
-  //   })
-  // }
-
-  // onError(err: number, msg: string): void {
-  //   console.error(err, msg)
-  // }
-
   onPressMpk = (url) => {
     if (!url) {
       return
@@ -175,6 +108,24 @@ export default class MediaPlayer
           onSearch={this.onPressMpk}
         />
         <br />
+        {isPlaying && (
+          <>
+            <Button
+              onClick={() => {
+                this.getMediaPlayer().pause()
+              }}
+            >
+              Pause
+            </Button>
+            <Button
+              onClick={() => {
+                this.getMediaPlayer().resume()
+              }}
+            >
+              Resume
+            </Button>
+          </>
+        )}
       </div>
     )
   }
